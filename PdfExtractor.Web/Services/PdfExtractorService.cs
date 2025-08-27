55acad6e2ac735ce90778b459d0fbaa480b11cf7
adchouaib <eladraoui.chouaib@gmail.com>
2025-08-27 23:03:41 +0100

    Chat interface

--- PdfExtractor.Web/Services/PdfExtractorService.cs
+++ PdfExtractor.Web/Services/PdfExtractorService.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using System.Text.Json;
+
 namespace PdfExtractor.Web.Services
 {
     public class PdfExtractorService
@@ -16,7 +19,28 @@ namespace PdfExtractor.Web.Services
 
             return documents;
         }
+
+        public async Task<MessageResponse?> SendMessage(string message, Guid documentId)
+        {
+            using StringContent jsonContent = new(
+                JsonSerializer.Serialize(new
+                {
+                    question = message
+                }),
+                Encoding.UTF8,
+                "application/json");
+
+            var response = await _client.PostAsync($"documents/{documentId}/answer", jsonContent);
+            if (response.IsSuccessStatusCode)
+            {
+                var documents = await response.Content.ReadFromJsonAsync<MessageResponse>();
+                return documents;
+            }
+
+            return null;
+        }
     }
 
     public record DocumentListResponse(string id, string fileName, string isProcessed);
+    public record MessageResponse(string response);
 }
